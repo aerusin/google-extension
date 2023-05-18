68ea041fcf8af2c2d03f750787f2904f2b1df0e0
Alex Rusin <thedarthal@gmail.com>
2023-05-18 09:06:04 -0400

    fixed it, now it displays live data!

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 let intervalId; // Variable to hold the interval ID
+
+app.use(cors()); // Enable CORS for all routes
+// Store the heart rate value globally
+let heartRateValue = null;
 
 // Define a route to start the data retrieval
 app.get('/start', async (req, res) => {
@@ -19,10 +24,9 @@ app.get('/start', async (req, res) => {
       return element && element.textContent > 0;
     });
 
-    // Start the interval to fetch the heartrate every second
+    // Start the interval to fetch the heart rate every second
     intervalId = setInterval(async () => {
-      const heartrate = await page.$eval('.heartrate', element => element.innerHTML);
-      console.log('Heartrate:', heartrate); // Do something with the heartrate
+      heartRateValue = await page.$eval('.heartrate', element => element.innerHTML);
     }, 1000); // Interval of 1 second (1000 milliseconds)
 
     res.json({ message: 'Data retrieval started' });
@@ -36,6 +40,11 @@ app.get('/start', async (req, res) => {
 app.get('/stop', (req, res) => {
   clearInterval(intervalId); // Clear the interval
   res.json({ message: 'Data retrieval stopped' });
+});
+
+// Define a route to retrieve the heart rate
+app.get('/heartrate', (req, res) => {
+  res.json({ heartRate: heartRateValue });
 });
 
 app.listen(PORT, () => {
--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,33 @@
-// Retrieve the heartrate data from the server every second
-setInterval(() => {
-    fetch('http://localhost:3000/start') // Include the full URL
-      .then(response => response.text())
-      .then(data => {
-        // Update the heartrate value in your script.js logic
-        console.log('Heartrate:', data); // Example: Log the heartrate value
-        // Use the heartrate value for further processing or display
-      })
-      .catch(error => {
-        console.error('Error fetching heartrate:', error);
-      });
-  }, 1000);
-  
+// Function to start the data retrieval
+function startDataRetrieval() {
+  // Make a request to start the data retrieval process on the server
+  fetch('http://localhost:3000/start')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.message);
+    })
+    .catch(error => {
+      console.error('Error starting data retrieval:', error);
+    });
+}
+
+// Function to retrieve and display the heart rate
+function getHeartRate() {
+  // Make a request to retrieve the heart rate from the server
+  fetch('http://localhost:3000/heartrate')
+    .then(response => response.json())
+    .then(data => {
+      const heartRate = data.heartRate;
+      // Update the heart rate value
+      document.getElementById('heartRateValue').innerText = heartRate !== null ? heartRate : '-';
+    })
+    .catch(error => {
+      console.error('Error retrieving heart rate:', error);
+    });
+}
+
+// Start the data retrieval process when the button is clicked
+document.getElementById('startButton').addEventListener('click', startDataRetrieval);
+
+// Fetch and display the heart rate every second
+setInterval(getHeartRate, 1000); // Interval of 1 second (1000 milliseconds)
